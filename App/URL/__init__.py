ef2ca96a7c32aa91651385bb500f586d532597a4
thomas-rodrigues <thomas.rodrigues@hbsis.com.br>
2020-07-15 09:26:25 -0300

    new url class

--- App/URL/__init__.py
+++ App/URL/__init__.py
@@ -1,16 +1,26 @@
 
 from settings import URL
+from App.Date import Date
 
 
 class Url:
 
     __url = URL
 
-    def __init__(self, data: str):
-        self.set_data(data)
+    def __init__(self, date: Date):
+        self.set_data(date)
 
-    def set_data(self, data: str):
-        self.__url = self.__url.replace("$$$$$$$$", data)
+    def set_data(self, date: Date):
+        day, month, year = date.get_date().day, date.get_date().month, date.get_date().year
+
+        if day < 10:
+            day = f"0{day}"
+
+        if month < 10:
+            month = f"0{month}"
+
+        self.__url = self.__url.replace("$$$$$$$$", f"{year}{month}{day}")
+        print(self.__url)
 
     def get_url(self):
         return self.__url
