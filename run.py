a28510e33d4378ee8c1de4b474168fe3ab0dc011
thomas-rodrigues <thomas.rodrigues@hbsis.com.br>
2020-07-15 09:26:01 -0300

    new run.py

--- run.py
+++ run.py
@@ -2,13 +2,18 @@
 from App.TxtConverter import TxtConverter
 from App.ExtractData import ExtractData
 from App.URL import Url
+from App.Date import Date
 
 
 if __name__ == '__main__':
 
-    date = input("Insira o ano, o mes e o dia - ex(20200703): ")
-    u = Url(date)
-    ext = ExtractData(u)
+    day = input("Insert day: ")
+    month = input("Insert month: ")
+    year = input("Insert year: ")
+    date = Date(day, month, year)
+    url = Url(date)
+    print(date.get_date())
+    ext = ExtractData(url)
     ext.extract()
     txt = TxtConverter(ext.get_data())
     txt.write()
