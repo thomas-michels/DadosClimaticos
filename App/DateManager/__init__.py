033dad6178f116a8c999308e20f3b5537721e288
thomas-rodrigues <thomas.rodrigues@hbsis.com.br>
2020-07-27 11:30:43 -0300

    Inserting new methods for DateManager

--- App/DateManager/__init__.py
+++ App/DateManager/__init__.py
@@ -1,49 +1,36 @@
 
 from App.Date import Date
 from App.Exceptions import UnprocessableEntityException
-from App.Date import Date
+import pendulum
+from datetime import datetime
 
 
 class DateManager:
 
-    _days_list = []
+    days_list = []
 
     def __init__(self, initial_date: Date, final_date: Date):
         self._initial_date = initial_date
         self._final_date = final_date
         self._check_dates()
-        self._generate_days_list()
 
     def _check_dates(self):
         if self._initial_date.get_date() > self._final_date.get_date():
             raise UnprocessableEntityException()
 
-    def _get_difference_days(self):
+    def generate_period(self):
+        initial_date = datetime(self._initial_date.get_date().year,
+                                self._initial_date.get_date().month,
+                                self._initial_date.get_date().day)
 
-        difference_days = 0
+        final_date = datetime(self._final_date.get_date().year,
+                              self._final_date.get_date().month,
+                              self._final_date.get_date().day)
 
-        if self._initial_date.get_date().month == self._final_date.get_date().month:
-            difference_days = self._final_date.get_date().day - self._initial_date.get_date().day
+        period = pendulum.period(initial_date, final_date)
 
-        elif self._initial_date.get_date().month != self._final_date.get_date().month:
-            pass
+        for days in period.range('days'):
+            self.days_list.append(Date(days.day, days.month, days.year))
 
-        return difference_days
-
-    def _generate_days_list(self):
-
-        if self._initial_date.get_date().month == self._final_date.get_date().month and\
-                self._initial_date.get_date().year == self._final_date.get_date().year:
-
-            day = self._final_date.get_date().day
-
-            while day != (self._initial_date.get_date().day - 1):
-                self._days_list.append(Date(day, self._final_date.get_date().month, self._final_date.get_date().year))
-                day -= 1
-
-        elif self._initial_date.get_date().month != self._final_date.get_date().month and\
-                self._initial_date.get_date().year == self._final_date.get_date().year:
-            pass
-
-        else:
-            raise UnprocessableEntityException("Over a very big period")
+    def get_days_list(self):
+        return self.days_list
